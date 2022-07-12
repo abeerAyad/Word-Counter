3e6d220bfcfcbf5f7222ec92be1d9c0a4a6aed9b
xAbeer <abeerayyad2000@gmail.com>
2022-07-12 17:16:11 +0300

    some  updates

--- main.js
+++ main.js
@@ -3,18 +3,24 @@ let text = document.getElementById('text');
 let msg =  document.getElementById('msg');
 let char = document.getElementById('char');
 let word = document.getElementById('word');
-let container = document.querySelector('.container')
+let container = document.querySelector('.container');
+let maxLength = text.getAttribute('maxLength')
+
 let test =`Word : 0`;
 text.addEventListener('focus',() => msg.style.cssText = `transform:translate(20px,-17px);`)
 
-text.addEventListener('keydown',(e) => {
-    let charactersText= e.target.value.trim();
-    let wordArray = charactersText.split(/[\n\r\s]+/g );
+text.addEventListener('input',(e) => {
+    let wordArray = text.value.trim().split(/[\n\r\s]+/g );
     word.innerHTML = `Word : ${wordArray.length}`;
-    char.innerHTML = `characters : ${charactersText.length}`;
-    console.log(charactersText.length)
-    if(charactersText.length === 0) word.innerHTML = test;
+    char.innerHTML = `characters : ${maxLength - text.value.length}`;
+    if(char.innerHTML ===`characters : 0`) {
+        text.classList.add('zero');
+        
+    } else {
+        text.classList.remove('zero');
+    }
 
 })
+
 
 window.open( container.style.transform = 'scale(1)')
